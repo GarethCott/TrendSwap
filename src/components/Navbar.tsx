ad04b36029364d65844110917eb65080d199a471
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-19 14:32:09 +0200

    Google Authentication completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,12 @@ import Link from 'next/link'
 import React from 'react'
 import { Icons } from './Icons'
 import { buttonVariants } from './ui/Button'
+import { getAuthSession } from '@/lib/auth'
 
-const Navbar = () => {
+const Navbar =  async () => {
+
+  const session = await getAuthSession()
+
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zince-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -17,7 +21,8 @@ const Navbar = () => {
 
         {/* searchBar */}
 
-        <Link href='/sign-in' className={buttonVariants()}>Sign In</Link>
+        {session ? (<p>your logged in</p>) : (<Link href='/sign-in' className={buttonVariants()}>Sign In</Link>)}
+
       </div>
     </div>
   )
