15c85875eedb31876be0aa6123eed3d06cce652c
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-20 18:13:16 +0200

    Finish with sub reddit page, start with sub reddit subscribing logic

---
+++ src/components/SubscribeLeaveToggle.tsx
@@ -0,0 +1,32 @@
+"use client"
+import {FC} from 'react'
+import { Button } from './ui/Button'
+import { useMutation } from '@tanstack/react-query'
+import { SubscribeToSubredditPayload } from '@/lib/validators/subreddit'
+import axios from 'axios'
+
+interface SubscribeLeaveToggleProps {
+  subreditId: string
+}
+
+const SubscribeLeaveToggle: FC<SubscribeLeaveToggleProps> = ({subreditId}) => {
+
+    const isSubscribed = false
+
+    const {} = useMutation({
+        mutationFn: async () => {
+            const payload: SubscribeToSubredditPayload = {
+                subreditId,
+            }
+            const {data} = await axios.post("/api/subreddit/subscribe", payload)
+            return data as string
+        },
+    })
+  return isSubscribed ? (
+    <Button className='w-full mt-1 mb-4'>Leave community</Button>
+  ) : (
+    <Button className='w-full mt-1 mb-4'>Join to post</Button>
+  )
+}
+
+export default SubscribeLeaveToggle
