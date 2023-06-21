1c43beffb1fcfcdf12042cb8e131f6819c718de1
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-21 21:30:21 +0200

    Finishing up with displaying of posts (still need to add display votes)

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -56,11 +56,11 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName}) => {
             if(index === posts.length - 1) {
                 return (
                     <li key={post.id} ref={ref}>
-                        <Post/>
+                        <Post commentAmt={post.comments.length} post={post} subredditName={post.subreddit.name}/>
                     </li>
                 )
             } else {
-                return <Post/>
+                return <Post commentAmt={post.comments.length} post={post} subredditName={post.subreddit.name}/>
             }
         })}
     </ul>
