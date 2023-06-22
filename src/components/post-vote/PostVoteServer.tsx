fd24c76c18893b0c0a53789f054cb924d3acadce
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-22 15:34:21 +0200

    Add PostVoteServer component to stream in data from server

--- src/components/post-vote/PostVoteServer.tsx
+++ src/components/post-vote/PostVoteServer.tsx
@@ -1,5 +1,7 @@
 import { Post, Vote, VoteType } from '@prisma/client'
-import { FC } from 'react'
+import {getServerSession} from 'next-auth'
+import { notFound } from 'next/navigation'
+import PostVoteClient from './PostVoteClient'
 
 interface PostVoteServerProps {
   postId: string,
@@ -8,15 +10,36 @@ interface PostVoteServerProps {
   getData?: () => Promise<(Post & {votes: Vote[]}) | null>
 }
 
-const PostVoteServer: FC<PostVoteServerProps> = ({
+const PostVoteServer = async ({
     postId, 
     initialVotesAmt,
     initialVote,
     getData,
-}) => {
+}: PostVoteServerProps) => {
 
+    const session = await getServerSession()
 
-  return <div>PostVoteServer</div>
+    let _votesAmt: number = 0
+
+    let _currentVote: VoteType | null | undefined = undefined
+
+    if(getData) {
+        const post = await getData()
+        if(!post) return notFound()
+
+        _votesAmt = post.votes.reduce((acc, vote)=> {
+            if(vote.type === 'UP') return acc +1
+            if(vote.type === 'DOWN') return acc -1
+            return acc
+        }, 0)
+
+        _currentVote = post.votes.find((vote)=> vote.userId === session?.user.id)?.type
+    } else {
+        _votesAmt = initialVotesAmt!
+        _currentVote = initialVote
+    }
+
+  return <PostVoteClient postId={postId} initialVotesAmt={_votesAmt} initialVote={_currentVote}/>
 }
 
 export default PostVoteServer
