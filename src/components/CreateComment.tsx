9052964362dea21be7c01636d7b2f619948306a4
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-22 21:50:00 +0200

    Finish with adding comments to posts

--- src/components/CreateComment.tsx
+++ src/components/CreateComment.tsx
@@ -5,16 +5,27 @@ import { Textarea } from './ui/TextArea'
 import { Button } from './ui/Button'
 import { useMutation } from '@tanstack/react-query'
 import { CommentRequest } from '@/lib/validators/comment'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
+import { toast } from '@/hooks/use-toast'
+import { useCustomToast } from '@/hooks/use-custom-toast'
+import { useRouter } from 'next/navigation'
+
+
 
 interface CreateCommentProps {
-  
+  postId: string,
+  replyToId?: string,
+
 }
 
-const CreateComment: FC<CreateCommentProps> = ({}) => {
+const CreateComment: FC<CreateCommentProps> = ({postId, replyToId}) => {
 
   const [input, setInput] = useState<string>('')
-  const {} = useMutation({
+  const {loginToast} = useCustomToast()
+  const router = useRouter()
+
+
+  const {mutate: comment, isLoading} = useMutation({
     mutationFn: async ({postId,text,replyToId}:CommentRequest)=> {
       const payload: CommentRequest = {
         postId,
@@ -24,6 +35,24 @@ const CreateComment: FC<CreateCommentProps> = ({}) => {
       const {data} = await axios.patch('/api/subreddit/post/comment', payload)
       return data
     },
+    onError: (err) => {
+      if(err instanceof AxiosError) {
+        if(err.response?.status=== 401) {
+          return loginToast()
+        }
+      }
+
+      return toast({
+        title: "There was a problem",
+        description: "Something went wrong, please try again.",
+        variant: "destructive",
+      })
+    },
+
+    onSuccess: () => {
+      router.refresh()
+      setInput('')
+    }
   })
 
   return (
@@ -33,7 +62,7 @@ const CreateComment: FC<CreateCommentProps> = ({}) => {
         <Textarea id='comment' value={input} onChange={(e)=> setInput(e.target.value)} rows={1} placeholder='What are your thoughts?'
         />
         <div className='mt-2 flex justify-end'>
-          <Button>Post</Button>
+          <Button isLoading={isLoading} disabled={input.length === 0} onClick={()=> comment({postId, text: input, replyToId})}>Post</Button>
         </div>
       </div>
     </div>
