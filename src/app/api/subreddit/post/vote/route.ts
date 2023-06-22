20c44757ee44ccabc215c403b0ef5e700302ebd2
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-22 12:42:53 +0200

    Introduce advanced caching with redis and Upstash

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -1,6 +1,9 @@
 import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
+import { redis } from "@/lib/redis"
 import { PostVoteValidator } from "@/lib/validators/Vote"
+import { CachedPost } from "@/types/redis"
+import { z } from "zod"
 
 const CACHE_AFTER_UPVOTES = 1
 
@@ -70,12 +73,59 @@ export async function PATCH(req: Request) {
             }, 0) 
 
             if(votesAmt >= CACHE_AFTER_UPVOTES) {
-                
+                const cachePayLoad: CachedPost = {
+                    authorUsername: post.author.username ?? '',
+                    content: JSON.stringify(post.content),
+                    id: post.id,
+                    title: post.title,
+                    currentVote: voteType,
+                    createdAt: post.createdAt,
+
+                }
+
+                await redis.hset(`post:${postId}`, cachePayLoad)
             }
+
+            return new Response('OK')
         }
 
+        await db.vote.create({
+            data: {
+                type: voteType,
+                userId: session.user.id,
+                postId,
+            }
+        })
+
+         // recount the votes
+        const votesAmt = post.votes.reduce( (acc, vote) => {
+            if(vote.type === 'UP') return acc + 1
+            if(vote.type === 'DOWN') return acc -1
+            return acc
+        }, 0) 
+
+        if(votesAmt >= CACHE_AFTER_UPVOTES) {
+            const cachePayLoad: CachedPost = {
+                authorUsername: post.author.username ?? '',
+                content: JSON.stringify(post.content),
+                id: post.id,
+                title: post.title,
+                currentVote: voteType,
+                createdAt: post.createdAt,
+
+            }
+
+            await redis.hset(`post:${postId}`, cachePayLoad)
+        }
+
+        return new Response('OK')
 
     } catch (error) {
+        if(error instanceof z.ZodError) {
+            return new Response("Invalid Vote request", {status: 422})
+        }
+
+        return new Response("Could not register your vote please try again", {status: 500})
         
     }
 }
