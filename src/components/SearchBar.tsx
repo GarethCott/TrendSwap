cbcf27805f6fa80b6f2750265928a414ee2181af
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-23 12:52:33 +0200

    Fix bug were searchbar doesnt close after clicking away

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,14 @@
 "use client"
-import { FC, useCallback, useState } from 'react'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from './ui/Command'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { Prisma, Subreddit } from '@prisma/client'
 import { CommandList } from 'cmdk'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { Users } from 'lucide-react'
 import debounce from 'lodash.debounce'
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
 
 interface SearchBarProps {
   
@@ -38,10 +39,19 @@ const SearchBar: FC<SearchBarProps> = ({}) => {
     }, [])
 
     const router = useRouter()
+    const commandRef = useRef<HTMLDivElement>(null)
 
+    useOnClickOutside(commandRef, ()=> {
+        setInput('')
+    })
+
+    const pathname = usePathname()
+    useEffect(()=> {
+        setInput('')
+    }, [pathname])
 
   return (
-    <Command className='relative rounded-lg border max-w-lg z-50 overflow-visible'>
+    <Command ref={commandRef} className='relative rounded-lg border max-w-lg z-50 overflow-visible'>
         <CommandInput 
         isLoading={isFetching}
         value={input}
