3d32600f364971c0e8b61d5234250108d3a63b66
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-23 12:21:26 +0200

    Finish search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,76 @@
-import { FC } from 'react'
+"use client"
+import { FC, useCallback, useState } from 'react'
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from './ui/Command'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { Prisma, Subreddit } from '@prisma/client'
+import { CommandList } from 'cmdk'
+import { useRouter } from 'next/navigation'
+import { Users } from 'lucide-react'
+import debounce from 'lodash.debounce'
 
 interface SearchBarProps {
   
 }
 
 const SearchBar: FC<SearchBarProps> = ({}) => {
+
+    const [input, setInput] = useState<string>('')
+
+    const {data: queryResults, refetch, isFetched, isFetching} = useQuery({
+        queryFn:async () => {
+            if(!input) return []
+            const {data} = await axios.get(`/api/search?q=${input}`)
+            return data as (Subreddit & {
+                _count: Prisma.SubredditCountOutputType
+            })[]
+        },
+        queryKey: ['search-query'],
+        enabled: false,
+    })
+    
+    const request =debounce(() => {
+        refetch()
+    })
+
+    const debounceRequest = useCallback(()=> {
+        request()
+    }, [])
+
+    const router = useRouter()
+
+
   return (
-    <div></div>
+    <Command className='relative rounded-lg border max-w-lg z-50 overflow-visible'>
+        <CommandInput 
+        isLoading={isFetching}
+        value={input}
+        onValueChange={(text) => {
+            setInput(text)
+            debounceRequest()
+        }}
+        className='outline-none border-none focus:border-none focus:outline-none ring-0' placeholder='Search communities...'/>
+
+        {input.length > 0 ? (
+            <CommandList className='absolute bg-white top-full inset-x-0 shadow rounded-b-md'>
+                {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+                {(queryResults?.length ?? 0) > 0 ? (
+                    <CommandGroup heading='Communities'>
+                        {queryResults?.map((Subreddit)=>(
+                            <CommandItem onSelect={(e)=> {
+                                router.push(`/r/${e}`)
+                                router.refresh()
+                            }}
+                            key={Subreddit.id} value={Subreddit.name}>
+                                <Users className='mr-2 h-4 w-4'/>
+                                <a href={`/r/${Subreddit.name}`}>r/{Subreddit.name}</a>
+                            </CommandItem>
+                        ))}
+                    </CommandGroup>
+                ): null}
+            </CommandList>
+        ) : null}
+    </Command>
  )
 }
 
